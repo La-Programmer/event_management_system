a360c4503b07d2d16ace7480f0252a99715c4c2c
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-09-04 09:57:50 +0100

    Setup base model

--- v1/db/engine.py
+++ v1/db/engine.py
@@ -6,11 +6,18 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 import os
 
 
 load_dotenv()
 Base = declarative_base()
+classes = {
+    # 'User': User,
+    # 'Event': Event,
+    # 'Invitation': Invitation
+}
 class DB:
     """Handle MySQL DB connection"""
     __engine = None
@@ -22,31 +29,89 @@ class DB:
         user = os.environ.get('USER')
         pwd = os.environ.get('PASS')
         host = os.environ.get('HOST')
+        env = os.environ.get('ENV')
         db = os.environ.get('DB')
-        # env = os.environ.get('ENV')
         self.__engine = create_engine(f"mysql+mysqldb://{user}:{pwd}@{host}/{db}")
+    
+    def get_object_by(self, obj_class_str, **kwargs):
+        """Gets an object from the DB by a particular attribute
+        """
+        obj_class = classes[obj_class_str]
+        db_session = self._session
+        raw_columns = obj_class.__table__.columns
+        columns = [str(field).split('.')[1] for field in raw_columns]
+        for i in kwargs:
+            if i not in columns:
+                raise InvalidRequestError
+        user = db_session.query(obj_class).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
+
 
     def save(self):
         """Commit all changes of the current DB session
         """
-        self.__session.commit()
+        try:
+            self.__session.commit()
+            print('Object save successfully')
+        except Exception as e:
+            raise e
+            
+    def new(self, new_obj):
+        """Adds a new object to the session DB
+        """
+        try:
+            self.__session.add(new_obj)
+        except Exception as e:
+            raise e
     
+    def update(self, obj, kwargs):
+        """Updates a DB record
+        """
+        name = obj.__class__.__name__
+        class_name = classes[name]
+        instance_object = self.__session.query(class_name).filter_by(obj.id).first()
+        for key, value in kwargs.items():
+            try:
+                setattr(instance_object, key, value)
+            except Exception as e:
+                raise e
+
+
     def reload(self):
         """Reloads data from the DB
         """
-        Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session
-        print('Database is up and running')
+        try:
+            Base.metadata.create_all(self.__engine)
+            sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+            Session = scoped_session(sess_factory)
+            self.__session = Session
+        except Exception as e:
+            raise e
     
     def is_alive(self):
         """Confirm that connecting to DB was successful
         """
         if self.__session and self.__engine:
-            print('DB connected and ready to go')
+            return True
+        return False
+    
+    def delete(self, obj):
+        """Delete from DB
+        """
+        if obj is not None:
+            try:
+                self.__session.delete(obj)
+            except Exception as e:
+                raise e
+        else:
+            raise ValueError('No object to delete')
     
     def close(self):
         """Call remove() method on the private session attribute
         """
-        self.__session.remove()
+        try:
+            self.__session.remove()
+        except Exception as e:
+            raise e
--- v1/models/base.py
+++ v1/models/base.py
@@ -8,6 +8,8 @@ from sqlalchemy import Column, String, DateTime
 from db import storage
 import uuid
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
 class BaseModel:
     """Base class for all classes in the system
     """
@@ -15,6 +17,60 @@ class BaseModel:
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now)
 
-    def __init__(self) -> None:
-        print('New base instance')
-        storage.is_alive()
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize base class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.now()
+            
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.now()
+            
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+    
+    def __str__(self):
+        """String representation of base class
+        """
+        return f"{self.__class__.__name__}.{self.id}: {self.__dict__}"
+    
+    def save_new(self):
+        """Saves an object to the DB
+        """
+        self.updated_at = datetime.now()
+        try:
+            storage.new(self)
+            storage.save()
+        except Exception as e:
+            raise e
+    
+    def update(self, **kwargs):
+        """Update a DB object
+        """
+        self.updated_at = datetime.now()
+        try:
+            storage.update(self, kwargs)
+            storage.save()
+        except Exception as e:
+            raise e
+        finally:
+            print(self)
+    
+    def delete(self, obj):
+        """Delete a DB object
+        """
+        try:
+            storage.delete(obj)
+        except Exception as e:
+            raise e
